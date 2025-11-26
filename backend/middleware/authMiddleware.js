6b5f36d9057f8c9c39db41a220c43640de178c72
akshq96 <akshitraj19@gmail.com>
2025-11-26 21:57:58 +0530

    adde

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const response = require('../utils/responseHandler');
+
+const authMiddleware = (req, res, next) => {
+    const authToken = req.cookies?.auth_token;
+    if (!authToken) {
+        return response(res, 401, false, 'Authentication token missing');
+    }
+    try {
+        const decode = jwt.verify(authToken, process.env.JWT_SECRET);
+        req.user = decode;
+        next();
+    } catch (error) {
+        console.error('JWT Verification Error:', error);
+        return response(res, 401, false, 'Invalid authentication token');
+    }
+}
